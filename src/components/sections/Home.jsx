049ea7c4a605a4930386e87258e095727cd8fefd
orodrigorezende <rodrigorsilva.rr94@gmail.com>
2025-05-09 22:50:24 -0300

    V1

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,31 @@
+import { RevealOnScroll } from "../RevealOnScroll";
+
+export const Home = () =>{
+    return (<section id="home"
+     className="min-h-screen flex items-center justify-center relative"
+     >
+        <RevealOnScroll>
+        <div className="text-center z-10 px-4">
+            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right">Olá , Eu sou o Rodrigo</h1>
+        
+
+            <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">
+
+                Sou estudante de Ciência da Computação na Universidade Federal de São João del-Rei (UFSJ) e atualmente estou no segundo período do curso. 
+                Tenho me dedicado a construir uma base sólida em programação, algoritmos e estruturas de dados, com foco especial em linguagens como C e JavaScript. 
+                Gosto de explorar na prática o que aprendo em sala de aula, seja através de projetos acadêmicos ou iniciativas pessoais. 
+                Estou especialmente interessado em desenvolvimento web e inteligência artificial, e busco constantemente novos desafios para crescer como desenvolvedor e profissional da área de tecnologia.
+            </p>
+            <div className="flex justify-center space-x-4">
+                <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]">
+                    Ver Projetos
+                </a>
+                <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:bg-blue-500/10 ">
+                    Contate-me
+                </a>
+            </div>
+        </div>
+        </RevealOnScroll>
+     </section>
+    );
+};
